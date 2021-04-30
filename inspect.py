918a4c82862ba1a2c858871373b74783f8b88c80
swoook <kimseongwoo21@gmail.com>
2021-04-30 18:00:18 +0900

    Add a feature for processing time benchmark

--- inspect.py
+++ inspect.py
@@ -16,7 +16,7 @@ import os
 import math
 import time
 
-class Demo(object):
+class Inspector(object):
     def __init__(self, config):
         self.config = config
         self.build_model()
@@ -58,14 +58,38 @@ class Demo(object):
         if not os.path.exists(output_dir): os.makedirs(output_dir)
         cv2.imwrite(output_path, multi_fuse)
 
+    def measure_fps(self, input_path):
+        input_data, _ = load_image_test(input_path)
+        with torch.no_grad():
+            input_data = torch.Tensor(input_data)
+            input_data = torch.unsqueeze(input_data, 0)
+            input_data = Variable(input_data)
+            if self.config.cuda:  input_data = input_data.cuda()
+            preds = self.net(input_data)
+        
+        time_s = time.perf_counter()
+        for _ in range(30):
+            preds = self.net(input_data)
+            pred = np.squeeze(torch.sigmoid(preds).cpu().data.numpy())
+            multi_fuse = 255 * pred
+        time_end = time.perf_counter()
+        inference_time = (time_end - time_s) / 10
+        print('FPS: {}'.format((1/inference_time)))
+        
+
 def main(config):
-    demo = Demo(config)
-    demo.infer(config.input_img_path, config.output_img_path)
+    inspector = Inspector(config)
+    if config.runmode == 'infer':
+        inspector.infer(config.input_img_path, config.output_img_path)
+    elif config.runmode == 'fps':
+        inspector.measure_fps(config.input_img_path)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--arch', type=str, default='resnet', help='resnet or vgg')
+    parser.add_argument('--runmode', type=str, choices=['infer', 'fps'], default='infer', 
+    help='infer: infer from single image and write a result as a .jpg file \n fps: measure a FPS of given model')
+    parser.add_argument('--arch', type=str, choices=['resnet', 'vggnet'], default='resnet', help='resnet or vgg')
     parser.add_argument('--input_img_path', metavar='DIR', required=True, help='Input image path')
     parser.add_argument('--model_path', metavar='DIR', required=True, help='.pth path to use in this demo')
     parser.add_argument('--output_img_path', metavar='DIR', required=True, 
